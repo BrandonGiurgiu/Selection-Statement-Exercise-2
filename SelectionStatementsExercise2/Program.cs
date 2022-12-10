57614c290167579a1a52736c20fb9dad71030fcf
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2022-12-09 19:50:55 -0700

    Exercise 2 Completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is a cool subject!");
+                    break;
+
+                case "band":
+                    Console.WriteLine("Band is the BEST subject!!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is so important to our future!");
+                    break;
+
+                case "lunch":
+                    Console.WriteLine("The yummiest subject in school!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Oh wow! I havn't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
+
         }
     }
 }
